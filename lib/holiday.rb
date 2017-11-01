b160dbbc322abd7f4185f390f9887831b5db9479
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 17:30:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_data|
     holiday_data.each do |holiday, supplies|
-      puts "#{season}: #{holiday}: #{supplies.join(", ")}"
+      puts "#{season}:n\ #{holiday}: #{supplies.join(", ")}"
     end
   end
 end
